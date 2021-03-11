2c1c48cb0c8884e37d96c3a02f65d12277d57e9f
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-11 14:46:10 -0500

    ListTile SOLO interfaz | 165

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Mapas Page")
+    return ListView.builder(
+      itemBuilder: (_,i){
+
+        return ListTile(
+          leading: Icon(Icons.map, color:Theme.of(context).primaryColor),
+          title: Text("httpaxell.conha"),
+          subtitle: Text("id:1"),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey,),
+          onTap: (){
+            print("Abrir algo");
+          },
+        );
+
+      },
+      itemCount: 10,  
     );
   }
 }
