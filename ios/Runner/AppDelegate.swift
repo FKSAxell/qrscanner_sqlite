e777d885f8b9dd9c1dbbdb9f278b4ff1a61a7d63
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-03-12 11:15:07 -0500

    Colocando Mapa y añadiendo APIs | 174

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,13 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCHR7zUoEf7YXJWz6gjHX1eVRxNvFD7Kfw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,20 +1,46 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qrscanner_sqlite/models/scan_model.dart';
-class MapaPage extends StatelessWidget {
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapaPage extends StatefulWidget {
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+
+   Completer<GoogleMapController> _controller = Completer();
+
+
+
+
   @override
   Widget build(BuildContext context) {
 
+    
 
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
-
+    final CameraPosition _kGooglePlex = CameraPosition(
+      target: scan.getLatLng(),
+      zoom: 17,
+      tilt: 50,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(
-        child: Text(scan.valor),
-      )
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
